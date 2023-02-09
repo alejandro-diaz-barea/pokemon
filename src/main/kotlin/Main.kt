68ca5f787b34fa972a58212a25be741bc6fcdd39
Alejandro <adiabar0510@g.educaand.es>
2023-02-09 16:41:01 +0100

    Resolucion de ticket 0002

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,7 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+    var pokemon1 = pokemon(12,"Juan",200,"juan")
+
+    println(pokemon1.toString())
 }
--- src/main/kotlin/pokemon.kt
+++ src/main/kotlin/pokemon.kt
@@ -1,4 +1,30 @@
 class pokemon(id:Int,nombre:String,vida:Int,tipo:String) {
-    
+    var id = 0
+        set(value) {
+            if(value<0){
+                throw Exception("El id no puede ser menor a 0")
+            }
+            else{
+                field = value
+            }
+        }
+    var nombre = " "
+    var vida = 0
+    var tipo = " "
+
+    init {
+        this.id = id
+        this.nombre = nombre
+        this.vida = vida
+        this.tipo = tipo
+        if(id<0){
+            throw Exception("El id no puede ser menor a 0")
+        }
+    }
+
+    override fun toString(): String {
+        return "Hola mi nombre es $nombre, tengo $vida de vida y soy de tipo $tipo"
+    }
+
 
 }
