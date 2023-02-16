594159c921b4c06c6c7492c7c5bc3fa44e0fcc96
Alejandro <adiabar0510@g.educaand.es>
2023-02-16 16:31:41 +0100

    He creado un set para el tipo de pokemon

--- src/main/kotlin/tipo.kt
+++ src/main/kotlin/tipo.kt
@@ -1,5 +1,18 @@
 class tipo(Tipo:String) {
+    var tipos = mutableListOf<String>("acero", "agua", "bicho", "dragon", "electrico", "fantasma", "fuego", "hada", "hielo", "lucha",
+        "normal", "planta", "psiquico", "roca", "siniestro", "tierra", "veneno", "volador")
     var tipo = " "
+        get() {
+            return field
+        }
+        set(value) {
+            if(value !in tipos){
+                IllegalArgumentException("El tipo no es válido")
+            }
+            else{
+                field = value 
+            }
+        }
     init {
         tipo = Tipo
     }
